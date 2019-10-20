f98f3a02f777928239175cf8fcc03bd0869fa66a
lawchen <lawchen@live.com>
2019-10-20 00:28:46 -0400

    Update VCF2GEN_LawStyle.bash
    Minor update to report the process steps in stdout
    - Added echo commands to report the process steps.

--- VCF2GEN_LawStyle.bash
+++ VCF2GEN_LawStyle.bash
@@ -17,14 +17,20 @@ OUT_FILE=$3                      # file_name_of_final_output_without_extension
 for f in ${VCFS}/*.vcf; do
   filename=$(basename "$f")
   filestem="${filename%%.*}"
+  echo "Converting $filname to $filestem.gen..."
   grep -v "#" $f | awk -F'\t' '{$6=$7=$8=$9=""; t=$2; $2=$3; $3=t; print $0}' | sed -r 's/ *[[:digit:]]+\|[[:digit:]]+\:[[:digit:]]+\.?[[:digit:]]*,[[:digit:]]+\.?[[:digit:]]*:[[:digit:]]+\.?[[:digit:]]*:/ /g' | sed 's/\,/ /g' > ${OUT_DIR}/$filestem.gen
 done
 
 # concatenate gen files to a single gen
 mkdir -p ${OUT_DIR}/COMPILED_GEN/
+echo "Concatenating all GEN files in ${OUT_DIR}/ to a single GEN file named ${OUT_FILE}.gen..."
 paste --delimiter=\\n --serial ${OUT_DIR}/*.gen > ${OUT_DIR}/COMPILED_GEN/${OUT_FILE}.gen
 
 # create sample file
+echo "Creating ${OUT_FILE}.sample..."
 cat <(echo -e "ID_1 ID_2 missing\n0 0 0") <(grep "#CHROM" $(ls ${VCFS}/*.vcf | head -1) | cut -f10- --output-delimiter=$'\n' | awk '{print $0,$0,0}') > ${OUT_DIR}/COMPILED_GEN/${OUT_FILE}.sample
 # Line below is meant for MAVAN, as Sanger binded Individual ID and Family ID from the sample ID in the VCF file
 # sed -i 's/_[0-9]*_R[0-9]*C[0-9]*//g' ${OUT_DIR}/COMPILED_GEN/${OUT_FILE}.sample # specifically replace duplicated sentrix IDs per ID column with single sentrix IDs
+
+# completion note
+echo "Process completed. The final GEN and SAMPLE files are located in ${OUT_DIR}/COMPILED_GEN/."
