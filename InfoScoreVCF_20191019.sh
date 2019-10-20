b38cc65c9d94f6cf69f8fc58c8b76b437e3ddf3e
lawchen <lawchen@live.com>
2019-10-19 23:24:37 -0400

    Update and rename InfoScoreVCF_20170609.sh to InfoScoreVCF_20191019.sh
    Major script update to allow the filter INFO score = 1
    - Previously, INFO score filter was set to keep variants with INFO > specified value. This is an issue if we only want INFO = 1. Thus, this update resolves the issue.

--- InfoScoreVCF_20191019.sh
+++ InfoScoreVCF_20191019.sh
@@ -2,8 +2,8 @@
 #$ -pwd
 
 # Developer: Lawrence M. Chen
-# Version/date: 20170609
-# Example: ./InfoScoreVCF_20170609.sh /share/projects/genodata/cohortA/sanger_imputation/cohortA.vcfs/ /share/projects/genodata/cohortA/post_imputation/info_filter/ cohortA_20170627 0.8
+# Version/date: 20191019
+# Example: ./InfoScoreVCF_20191019.sh /share/projects/genodata/cohortA/sanger_imputation/cohortA.vcfs/ /share/projects/genodata/cohortA/post_imputation/info_filter/ cohortA_20170627 0.8
 
 USAGE="$(basename "$0") is a program to filter out genetic variants in the human autosomes that fail to meet the INFO score criterion from VCF files.
 \n
@@ -18,7 +18,9 @@ The four mandatory arguments must be specified in the indicated order:
 \n\t    path_to_folder_with_vcfs         \t\t location of the input vcf files
 \n\t    path_to_new_vcfs               \t\t\t location of the output files
 \n\t    root_name_of_output_dataset      \t\t part of the output file names to help identify the output files (e.g., cohort name and date)
-\n\t    info_filter                  \t\t\t\t numeric value between 0 and 1 for the info score filter
+\n\t    info_filter                  \t\t\t\t numeric value between 0 and 1 for the info score filter;
+\n\t                               \t\t\t\t\t genetic variants with info score greater than the specified numeric value will be retained;
+\n\t                               \t\t\t\t\t however, if value is 1, then only genetic variants with info score equal to 1 will be retained
 \n
 "
 
@@ -39,14 +41,56 @@ INFO=$4                          # info_filter (e.g., 0.8)
 
 mkdir -p ${OUT_DIR}/pbs_jobs/logdir
 
-for CHR in $(seq 1 22); do
+if (( $(bc <<<"$INFO == 1") )); then
+  echo "INFO = 1 will be retained."
 
-  f=${VCFS}/${CHR}.vcf$(if [ -a ${VCFS}/${CHR}.vcf ]; then : ; else echo .gz ; fi)
-  filename=$(basename "$f")
-  filestem="${filename%%.*}"
-  echo $filestem
+  for CHR in $(seq 1 22); do
 
-  cat > ${OUT_DIR}/pbs_jobs/InfoScoreVCF_${CHR}.bash << EOT # log InfoScoreVCF.sh job information for each chromosome
+    pbs=${OUT_DIR}/pbs_jobs/InfoScoreVCF_info1_${CHR}.pbs
+    f=${VCFS}/${CHR}.vcf$(if [ -a ${VCFS}/${CHR}.vcf ]; then : ; else echo .gz ; fi)
+    filename=$(basename "$f")
+    filestem="${filename%%.*}"
+    echo $filestem
+
+    cat > $pbs << EOT # write pbs script information for each chromosome
+#!/bin/bash
+#PBS -q batch
+#PBS -N VCF_info1_$CHR
+#PBS -o ${OUT_DIR}/pbs_jobs/logdir
+#PBS -e ${OUT_DIR}/pbs_jobs/logdir
+#PBS -l walltime=02:00:00
+#PBS -l nodes=1:ppn=4
+
+####### JOB CODE #######
+
+cd ${OUT_DIR}
+
+echo "Job submission date: $(date +%c)"
+echo "path_to_folder_with_vcfs = ${VCFS}/"
+echo "path_to_new_vcfs = ${OUT_DIR}/"
+echo "root_name_of_output_dataset = $DAT"
+echo "filestem = $filestem"
+echo "INFO = 1"
+
+bcftools --version
+bcftools view -Ou -i 'INFO=1' $f -o ${OUT_DIR}/${DAT}_info1_chr${filestem}.vcf -O v
+EOT
+    chmod 754 $pbs
+    qsub $pbs
+  done | tee -a ${OUT_DIR}/${DAT}_$(basename "${0%.*}")_$(date +%Y%m%d).log
+
+else
+  echo "INFO > $INFO will be retained."
+
+  for CHR in $(seq 1 22); do
+
+    pbs=${OUT_DIR}/pbs_jobs/InfoScoreVCF_info0${INFO#*.}_${CHR}.pbs
+    f=${VCFS}/${CHR}.vcf$(if [ -a ${VCFS}/${CHR}.vcf ]; then : ; else echo .gz ; fi)
+    filename=$(basename "$f")
+    filestem="${filename%%.*}"
+    echo $filestem
+
+    cat > $pbs << EOT # write pbs script information for each chromosome
 #!/bin/bash
 #PBS -q batch
 #PBS -N VCF_info0${INFO#*.}_$CHR
@@ -69,9 +113,11 @@ echo "INFO > ${INFO}"
 bcftools --version
 bcftools view -Ou -i 'INFO>${INFO}' $f -o ${OUT_DIR}/${DAT}_info0${INFO#*.}_chr${filestem}.vcf -O v
 EOT
-  chmod 754 ${OUT_DIR}/pbs_jobs/InfoScoreVCF_${CHR}.bash
-  qsub ${OUT_DIR}/pbs_jobs/InfoScoreVCF_${CHR}.bash
-done | tee -a ${OUT_DIR}/${DAT}_$(basename "${0%.*}")_$(date +%y%m%d).log
+    chmod 754 $pbs
+    qsub $pbs
+  done | tee -a ${OUT_DIR}/${DAT}_$(basename "${0%.*}")_$(date +%Y%m%d).log
+
+fi
 
 bcftools --version | tee -a ${OUT_DIR}/${DAT}_$(basename "${0%.*}")_$(date +%y%m%d).log
 smallestVCF=$(ls -rS ${VCFS}/*.vcf$(if [ -a ${VCFS}/1.vcf.gz ]; then echo .gz ; fi) | head -1 )
